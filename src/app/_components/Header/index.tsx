5bd62942b59d1886ce3e0880c9a99acd4e5cb626
Maciej Bednarczyk <madecki1991@gmail.com>
2024-05-31 12:22:14 +0200

    upgrade packages and fix component block types

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -2,15 +2,12 @@
   /* eslint-disable @next/next/no-img-element */
 }
 
-import React from 'react'
-import Link from 'next/link'
-
-import { Header } from '../../../payload/payload-types'
+import { Header as HeaderType } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
 import HeaderComponent from './HeaderComponent'
 
 export async function Header() {
-  let header: Header | null = null
+  let header: HeaderType | null = null
 
   try {
     header = await fetchHeader()
